83c0ac26ba83d1b51d38871d58ea75b333a049f4
woodminus <160528767+woodminus@users.noreply.github.com>
2022-02-24 20:21:40 +0000

    build changes

--- Code/model.py
+++ Code/model.py
@@ -41,4 +41,40 @@ def resnext(layer):
         extractor.add_module('2', net.relu)
         extractor.add_module('3', net.maxpool)
         extractor.add_module('4', net.layer1)
-        extractor.add_mod
+        extractor.add_module('5', net.layer2)
+        extractor.add_module('6', net.layer3)
+    else:
+        extractor = []
+    return extractor
+
+
+# return backbone extractor based on nn_type and layer in configs
+def get_backbone_extractor():
+    if nn_type == 'vgg':
+        return vgg16(layer).cuda(device_ids[0])
+
+    if nn_type == 'resnext':
+        return resnext(layer).cuda(device_ids[0]).eval()
+
+    error_message('Failed to get backbone extractor. \nInput nn_type: {}'.format(nn_type))
+
+
+# return visual concept centers
+def get_vc(dataset_override=None):
+    if dataset_override != None:
+        dataset = dataset_override
+        vMF_kappa_ = vMF_kappas['{}_{}_{}'.format(nn_type, layer, dataset_override)]
+    else:
+        dataset = dataset_train
+        vMF_kappa_ = vMF_kappa
+    vc = np.zeros((vc_num, feature_num))
+
+    file_name = meta_dir + 'ML_{0}/dictionary_{0}_{1}/dictionary_{2}_{3}_kappa{4}.pickle'.format(nn_type, dataset, layer, vc_num, vMF_kappa_)
+    try:
+        vc = np.load(file_name, allow_pickle=True)
+    except:
+        error_message('Failed to load VC. \nInput filename: {}'.format(file_name))
+    
+    vc = vc[:, :, np.newaxis, np.newaxis]
+    vc = torch.from_numpy(vc).type(torch.FloatTensor)
+    return vc.c
