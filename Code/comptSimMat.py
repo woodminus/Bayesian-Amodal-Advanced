cd984c044be67b36efc052bbab63962620556f9a
woodminus <160528767+woodminus@users.noreply.github.com>
2022-02-24 11:41:43 +0000

    yarn upgrade

--- Code/comptSimMat.py
+++ Code/comptSimMat.py
@@ -62,4 +62,38 @@ for category in categories:
 		act_per_pix = np.zeros(nthresh)
 		layer_feature_b = [None for nn in range(100)]
 		magic_thhs = np.asarray([x*1/nthresh for x in range(nthresh)])
-		for idx,magic_thh in en
+		for idx,magic_thh in enumerate(magic_thhs):
+			for nn in range(100):
+				layer_feature_b[nn] = (r_set[nn]<magic_thh).astype(int).T
+				coverage[idx] 	+= np.mean(np.sum(layer_feature_b[nn],axis=0)>0)
+				act_per_pix[idx] += np.mean(np.sum(layer_feature_b[nn],axis=0))
+		coverage=coverage/100
+		act_per_pix=act_per_pix/100
+		best_loc=(act_per_pix>2)*(act_per_pix<15)
+		if np.sum(best_loc):
+			best_thresh = np.min(magic_thhs[best_loc])
+		else:
+			best_thresh = 0.45
+		layer_feature_b = [None for nn in range(N)]
+		for nn in range(N):
+			layer_feature_b[nn] = (r_set[nn]<best_thresh).astype(int).T
+
+		print('Start compute sim matrix ... magicThresh {}'.format(best_thresh))
+		_s = time.time()
+
+		mat_dis1 = np.ones((N,N))
+		mat_dis2 = np.ones((N,N))
+		N_sub = 200
+		sub_cnt = int(math.ceil(N/N_sub))
+		for ss1 in range(sub_cnt):
+			start1 = ss1*N_sub
+			end1 = min((ss1+1)*N_sub, N)
+			layer_feature_b_ss1 = layer_feature_b[start1:end1]
+			for ss2 in range(ss1,sub_cnt):
+				print('iter {1}/{0} {2}/{0}'.format(sub_cnt, ss1+1, ss2+1))
+				_ss = time.time()
+				start2 = ss2*N_sub
+				end2 = min((ss2+1)*N_sub, N)
+				if ss1==ss2:
+					inputs = [(layer_feature_b_ss1, nn) for nn in range(end2-start2)]
+					para_rst = np.array(Parallel(n_jobs=paral_num)(delayed(vc_dis
